b3c8f94f2728d52b173cb41cb987cb9167c28068
Matthieu Pauze <matthieu.pauze@beslogic.com>
2023-02-09 14:24:25 -0500

    add text

--- ngrx.js
+++ ngrx.js
@@ -1,6 +1,7 @@
 const list = ["effects", "reducers", "selectors", "store", "actions"];
+console.log("You seem to be having problems troubleshooting your NGRX state management...");
 
 setInterval(() => {
   let random = Math.floor(Math.random() * 5);
-  console.log(`Have you looked at the ${list[random]}?`);
+  console.log(`Have you tried looking at the ${list[random]}?`);
 }, 4000);
